afb140b0f8517ccae3978ccae137ea871ca9ac34
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-23 14:48:35 +0200

    exercice1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,26 @@
+
+//Tp1-Question1
 function calculerMoyenne() {
-    let dc1 = parseFloat(document.getElementById("dc1").value);
-    let dc2 = parseFloat(document.getElementById("dc2").value);
-    let ds = parseFloat(document.getElementById("ds").value);
+    let dc1 = document.getElementById("dc1").value;
+    let dc2 = document.getElementById("dc2").value;
+    let ds = document.getElementById("ds").value;
 
     let moyenne = (dc1 + dc2 + ds) / 3;
 
     document.getElementById("moyenne").innerHTML = "La moyenne est : " + moyenne;
 }
+//Tp1-question2
+function calculerNombreJours() {
+    let mois =document.getElementById("mois").value;
+    let annee =document.getElementById("annee").value;
+
+    let nombreJours = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (annee % 4 == 0 ){
+        nombreJours[1] = 29;
+    }
+    let resultat = "Le mois " + mois + " de l'annee " + annee + " a " + nombreJours[mois - 1] + " jours.";
+
+    document.getElementById("resultat").innerHTML = resultat;
+}
+//Tp1-question3
